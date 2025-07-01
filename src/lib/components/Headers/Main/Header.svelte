8ac20db2ce238f676fef2b3b93e17c7f4f5e616a
Computerly <caleb0515@live.com>
2025-07-01 17:40:53 -0500

    updated header

--- src/lib/components/Headers/Main/Header.svelte
+++ src/lib/components/Headers/Main/Header.svelte
@@ -4,6 +4,7 @@
   import { clsx } from "clsx";
   import { navScroll } from "./scroll";
   import { page } from "$app/state";
+  import underline from "$lib/assets/underline.svg";
 
   let allowCollapse = $derived(page.data?.allowNavigationCollapse ?? false);
 
@@ -12,28 +13,36 @@
   let marker: HTMLDivElement;
 
   function moveMarker(e: MouseEvent) {
-    if (!marker || !e.target) return;
-
-    const target = e.target as HTMLElement;
-    const elWidth = target.offsetWidth; //e.target.clientWidth;
-    console.log(target.offsetTop);
-
-    marker.style.left = `${target.offsetLeft + elWidth / 2 - marker.offsetWidth / 2}px`;
-    marker.style.top = `${target.offsetTop}px`;
+    // if (!marker || !e.target) return;
+    // const target = e.target as HTMLElement;
+    // const elWidth = target.offsetWidth; //e.target.clientWidth;
+    // console.log(target.offsetTop);
+    // marker.style.left = `${target.offsetLeft + elWidth / 2 - marker.offsetWidth / 2}px`;
+    // marker.style.top = `${target.offsetTop}px`;
     // marker.style.width = `${e.target.offsetWidth}px`;
   }
 </script>
 
 {#snippet link(text: string, href: string, className: string = "")}
-  <li>
-    <a
-      {href}
-      class={cn(
-        "text-sm py-1 px-3 transition-all hover:bg-carbon-500/50 border  rounded-md border-carbon-500 bg-carbon-950",
-        className,
-      )}
-      onclick={(e) => moveMarker(e)}>{text}</a
-    >
+  <li class="group">
+    <a {href} class={cn("block text-sm px-4 py-2", className)}>
+      <div class="relative">
+        <div
+          class="font-weight-fix-medium group-hover:font-medium w-fit"
+          data-text={text}
+        >
+          {text}
+        </div>
+
+        <img
+          src={underline}
+          alt="underline"
+          class="underline_int"
+          aria-hidden="true"
+        />
+        <!-- invert scale-x-0 translate-y-full absolute bottom-0 duration-200 h-2 transition-all origin-bottom-left hover:origin-bottom-right group-hover:scale-x-100 -->
+      </div>
+    </a>
   </li>
 {/snippet}
 
@@ -63,21 +72,23 @@
       /></a
     >
 
-    <ul class="flex items-center gap-4">
+    <ul class="flex items-center gap-0">
       {@render link("Services", "/services")}
       {@render link("Pricing", "/pricing")}
       {@render link("About", "/about")}
     </ul>
 
     <ul class="place-self-end self-center">
-      {@render link("Start now", "/contact", "bg-patina-500")}
-      <!-- <a href="/contact" class="btn btn-primary font-medium text-white">Start now</a> -->
+      <!-- {@render link("Start now", "/contact", "bg-patina-500")} -->
+      <li>
+        <a href="/contact" class="btn btn-secondary">Start now</a>
+      </li>
     </ul>
   </nav>
-  <div
+  <!-- <div
     bind:this={marker}
     class="absolute transition-all duration-750 top-0 translate-y-[calc(100%+0.5rem)] left-0 bg-patina-500 select-none w-6 h-1 rounded-xs"
-  ></div>
+  ></div> -->
 
   <div
     class="border-carbon-500 border-t absolute bottom-0 min-w-0 left-1/2 -translate-x-1/2 transition-all {$scrollState.hasScrolled
@@ -85,3 +96,23 @@
       : 'w-0'}"
   ></div>
 </header>
+
+<style>
+  .underline_int {
+    filter: invert(100%);
+    transform: translateY(100%) scaleX(0);
+    bottom: 0;
+    left: 0;
+    position: absolute;
+    height: calc(var(--spacing) * 2);
+    transition: all;
+    transition-duration: 200ms;
+    transform-origin: bottom left;
+    transition-timing-function: ease-in;
+
+    li:hover &,
+    &.active {
+      transform: translateY(100%) scaleX(100%);
+    }
+  }
+</style>
