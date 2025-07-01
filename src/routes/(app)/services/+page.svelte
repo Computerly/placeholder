e1094cfb67179054515a68d2d4bacd5e3fda73dc
Computerly <caleb0515@live.com>
2025-07-01 17:44:02 -0500

    added services hero

--- src/routes/(app)/services/+page.svelte
+++ src/routes/(app)/services/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import SEO from "$lib/components/SEO/SEO.svelte";
+  import Hero from "./components/Hero.svelte";
 </script>
 
 <SEO
@@ -7,4 +8,9 @@
   description="Discover the digital services Rebricks Digital offers. From custom web design to performance optimization. Built to elevate your brand and drive results."
 />
 
-<div class="pt-nav mx-sides">services</div>
+<div class="bg-carbon-950 min-h-[500vh]">
+  <Hero />
+  <div class="p-16">
+    <button class="outline text-carbon-500">Button Text</button>
+  </div>
+</div>
---
+++ src/routes/(app)/services/components/Hero.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { book_a_meeting_url } from "$lib/config";
+</script>
+
+<div
+  class="min-h-[90dvh] grid place-items-center-safe shadow-base-100 rounded-b-4xl bg-patina-500"
+>
+  <div class="mx-sides grid items-center text-center justify-items-center-safe">
+    <div class="badge badge-neutral mb-2">Digital Services for Business</div>
+    <div class="mb-8">
+      <h1
+        class="text-6xl leading-tight
+		   mb-4 font-medium"
+      >
+        Unlock the power<br /> of 🕑 24/7, 🗓️ 365<br /> sales staff
+      </h1>
+      <p class="text-carbon-50 max-w-prose">
+        Stop treating your website as a virtual brochure. It is the first
+        salesmen your customers interact with, and the first impression of your
+        company.
+      </p>
+    </div>
+    <div class="flex gap-2 items-center">
+      <a href="/contact" class="btn btn-ghost">Find something</a>
+      <a href={book_a_meeting_url} class="btn btn-secondary">Book a call</a>
+    </div>
+  </div>
+</div>
